acad9b486fde20c93f95489a7e5025b6703c500b
Shasuna Lin <162057763+shasunal@users.noreply.github.com>
2025-07-25 19:38:45 -0400

    Fix image path for GitHub Pages subdirectory

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
      <header className="header">
   <img
-    src="/shasunaphoto.jpg"
+     src={`${process.env.PUBLIC_URL}/shasunaphoto.jpg`}
     alt="Shasuna Lin"
     className="profile-photo"
   />
